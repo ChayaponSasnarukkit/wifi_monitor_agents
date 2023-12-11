5658c2938a31812a8308d2cd6a65e67405472ba9
S.Chayapon <chayapon.sa@ku.th>
2023-12-11 18:36:57 +0700

    add configure_ap

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from typing import Optional
 from schemas import ConfigureClientData, ConfigureAccessPointData
 from utils.generate_configure_scripts import generate_client_script, generate_ap_script
-from utils.run_subprocess import run_subprocess, is_client_config_active
+from utils.run_subprocess import run_subprocess, is_client_config_active, is_ap_config_active
 import uvicorn
 from contextlib import asynccontextmanager
 import asyncio
@@ -27,7 +27,7 @@ def index():
     return {"message": "hello world"}
 
 @app.post("/configure/client")
-async def configure(request_body: ConfigureClientData):
+async def configure_client(request_body: ConfigureClientData):
     # ตรวจสอบว่ามีกำลังรัน simulate_process อยู่หรือไม่
     if app.simulate_process is not None:
         raise HTTPException(400, "simulate_process is running")
@@ -57,9 +57,33 @@ async def configure(request_body: ConfigureClientData):
 
 
 @app.post("/configure/ap")
-def configure(request_body: ConfigureAccessPointData):
-    print(request_body)
-    return generate_ap_script(request_body)
+async def configure_ap(request_body: ConfigureAccessPointData):
+    # ตรวจสอบว่ามีกำลังรัน simulate_process อยู่หรือไม่
+    if app.simulate_process is not None:
+        raise HTTPException(400, "simulate_process is running")
+    
+    # ตรวจสอบว่า ConfigureClientData ที่ได้มากำลังใช้งานอยู่หรือป่าว ถ้าใช้งานอยู่แล้วให้ตอบกลับไปเลยว่าพร้อมใช้งาน
+    if await is_ap_config_active(request_body):
+        return {"message": "wifi is connected"}
+    
+    # หากไม่จะต้องแก้ configuration
+    # acquire the lock
+    async with app.writing_configure_lock:
+    # critical section
+        # reset the configuration
+        await run_subprocess("cp ./default_config/wireless /etc/config/wireless")
+        # configure the new configuration
+        await run_subprocess(generate_ap_script(request_body))
+    # lock is released automatically...
+    
+    # polling check if wifi is connected with timeout 10 sec
+    cnt = 0
+    while cnt < 30:
+        if await is_ap_config_active(request_body):
+            return {"message": "wifi is connected"}
+        await asyncio.sleep(5)
+        cnt += 1
+    raise HTTPException(400, "Take too much time to enable wifi (AP is not sending ssid broadcast)")
 
 @app.post("/configure/client/get_configure_script")
 def get_client_configure_script(request_body: ConfigureClientData):
--- utils/run_subprocess.py
+++ utils/run_subprocess.py
@@ -23,12 +23,28 @@ async def is_client_config_active(request_body: ConfigureClientData) -> bool:
         stdout, stderr = await run_subprocess("iw dev wlan1 link")
     return _is_client_config_active(stdout, request_body.ssid)
     
-    
+def _is_ap_config_active(link_status: str, ssid: str, tx_power: int, request_body: ConfigureAccessPointData) -> bool:
+    if int(link_status[test_ap_status.find("TX packets:") + 11]) > 0 and ssid == request_body.ssid and tx_power == request_body.tx_power:
+        return True
+    else:
+        return False
+    pass
+
+async def is_ap_config_active(request_body: ConfigureAccessPointData) -> bool:
+    if request_body.radio == "5G":
+        link_status, _ = await run_subprocess("ifconfig wlan0")
+        ssid, _ = await run_subprocess("uci get wireless.AP_radio0.ssid")
+        tx_power, _ = await run_subprocess("uci get wireless.AP_radio0.txpower")
+    else:
+        link_status, _ = await run_subprocess("ifconfig wlan1")
+        ssid, _ = await run_subprocess("uci get wireless.AP_radio1.ssid")
+        tx_power, _ = await run_subprocess("uci get wireless.AP_radio1.txpower")
+    return _is_ap_config_active(link_status, ssid, tx_power, request_body)   
             
     
     # from utils.run_subprocess import check_inuse_client_config
 
-test_status = """iw dev wlan0 link
+test_client_status = """iw dev wlan0 link
 Connected to 68:7f:74:3b:b0:01 (on wlan0)
         SSID: tesla-5g-bcm
         freq: 5745
@@ -36,3 +52,15 @@ Connected to 68:7f:74:3b:b0:01 (on wlan0)
         TX: 4084 bytes (23 packets)
         signal: -31 dBm
         tx bitrate: 300.0 MBit/s MCS 15 40Mhz short GI"""
+        
+test_ap_status = """
+wlan0     Link encap:Ethernet  HWaddr 00:25:9C:13:D2:3C
+          inet addr:192.168.0.121  Bcast:192.168.0.255  Mask:255.255.255.0
+          inet6 addr: fe80::225:9cff:fe13:d23c/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:222 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:132 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000
+          RX bytes:42984 (41.9 KiB)  TX bytes:13259 (12.9 KiB)"""
+          
+# print(test_ap_status.split().find("TX packets:"))
